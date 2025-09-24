6d68632c857a6f0919c05569612ef3581f216361
vivian <mookbot@gmail.com>
2025-09-23 18:43:37 -0700

    add lib folder from someone else this seems good

---
+++ .config/nixos/lib/default.nix
@@ -0,0 +1,45 @@
+{ lib }:
+# stolen from https://github.com/gvolpe/nix-config/blob/9cf395d695093074b77a234dd449c07fb4c07d09/lib/default.nix
+# under apache licence, no changes as of inital commit
+{
+  /* Return first binary executable name of the given derivation
+     Type:
+       exe :: Derivation -> String
+  */
+  exe = drv:
+    let
+      regFiles = lib.mapAttrsToList (f: _: f) (lib.filterAttrs (_: t: t == "regular") (builtins.readDir "${drv}/bin"));
+      mainProg = drv.meta.mainProgram or (lib.head regFiles);
+    in
+    "${drv}/bin/${mainProg}";
+
+  /* Return the input string with any newline marker removed.
+     Type:
+       removeNewline :: String -> String
+  */
+  removeNewline = lib.replaceStrings [ "\n" ] [ "" ];
+
+  /* Return either `emptyValue` or `fileAction filepath`. Both need to be of the same type.
+
+     Example:
+       token = lib.secretManager {
+         filepath = ./secrets/access-token;
+         fileAction = file: lib.removeNewline (lib.readFile file);
+         encryptedSha256 = lib.fakeSha256;
+         emptyValue = "SECRET";
+       };
+
+     Type:
+       secretManager :: String -> (String -> a) -> String -> a -> a
+  */
+  secretManager =
+    { filepath
+    , fileAction ? import
+    , encryptedSha256
+    , emptyValue
+    }:
+    let
+      fileHash = builtins.hashFile "sha256" filepath;
+    in
+    if fileHash == encryptedSha256 then emptyValue else (fileAction filepath);
+}
