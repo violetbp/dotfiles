33d78125bb44ad97c0b25b8bb0bbfb9690007457
vivian <mookbot@gmail.com>
2025-09-23 19:37:07 -0700

    this might break everything

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -1,9 +1,30 @@
 
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-  inputs.home-manager.url = github:nix-community/home-manager;
-  
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+    home-manager.url = github:nix-community/home-manager;
+    flake-schemas.url = github:DeterminateSystems/flake-schemas;
+  };
   outputs = { self, nixpkgs, ... }@attrs: {
+    
+    let
+      system = "x86_64-linux";
+
+      overlays = import ./lib/overlays.nix { inherit inputs system; };
+
+      pkgs = import nixpkgs {
+        inherit overlays system;
+        config.allowUnfree = true;
+      };
+
+      neovim = self.homeConfigurations.niri-hdmi.config.programs.neovim-ide.finalPackage;
+    in{
+      schemas =
+        inputs.flake-schemas.schemas //
+        import ./lib/schemas.nix { inherit (inputs) flake-schemas;
+      };
+    };
+
     nixosConfigurations = {
       terra = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
